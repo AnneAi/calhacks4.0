061df5bffeaaaaf22c7e4cea99d6d662b240b50e
Baboo7 <baptiste.studer@laposte.net>
2017-10-28 21:51:16 -0700

    bootstrap application w/ uuid service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
+import { UuidService } from './services/uuid.service'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ],
+  providers: [ UuidService ]
 })
 export class AppComponent { }
