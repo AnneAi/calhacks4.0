e066a261f264d49b4deb2d3a0566324578187396
Baboo7 <baptiste.studer@laposte.net>
2017-10-28 21:52:58 -0700

    request agent on mcq done

--- src/app/chat/messages/MCQ/mcq.component.ts
+++ src/app/chat/messages/MCQ/mcq.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 
+import { DialogFlowService } from '../../../services/dialog-flow.service';
+
 @Component({
   selector: 'mcq-msg',
   templateUrl: './mcq.component.html',
@@ -17,9 +19,17 @@ export class MCQComponent {
   private studentAnswer: number;
   private success: boolean;
 
+  constructor(private dialogFlowService: DialogFlowService) { }
+
   checkAnswer(i: number): void {
     this.answered = true;
     this.studentAnswer = i;
     this.success = i == this.answer;
+
+    if (this.success) {
+      this.dialogFlowService.sendMessage('get it');
+    } else {
+      this.dialogFlowService.sendMessage('get it');
+    }
   }
 }
